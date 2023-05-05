bc7be9a94d002e39b7183d890c10b875bf309662
justin <justin.suess@westpoint.edu>
2023-05-05 11:42:43 -0400

    modified stuff

--- project/p4rt-src/switch.py
+++ project/p4rt-src/switch.py
@@ -114,6 +114,12 @@ def ProcPacketIn(switch_name, logs_dir, num_logs_threshold):
 
 
                         #### ADD YOUR CODE HERE ... ####
+                        if eth_type == ETH_TYPE_VLAN:
+                            #install a flow entry to forward packets based onegress port
+                        else:
+                            #Install a flow entry to forward packets based on egress port
+                            table_entry = p4.sh.TableEntry('MyIngress.switch_table')
+                            table_entry.match
                         pass
 
 
